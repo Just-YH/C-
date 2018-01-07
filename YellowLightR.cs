d9766532a6f5b17ef94f1bdf343570d4ccc53f10
GurinovichEV <zik_zhenja@mail.ru>
2018-01-07 22:47:17 +0300

    lab 5

---
+++ YellowLightR.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    public class YellowLightR : ITrafficLight
+    {
+        public void Change(TrafficLight light)
+        {
+            light.State = new RedLight();
+        }
+
+        public void ReportState()
+        {
+            Console.WriteLine("Yellow Light");
+        }
+    }
+}
